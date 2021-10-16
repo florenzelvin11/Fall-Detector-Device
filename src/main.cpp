3b8733c668ebc0ef6d5a04e7d03d03a634bf3659
florenzelvin11 <florenzmanaois@gmail.com>
2021-10-16 14:56:45 +1100

    name change

--- src/main.cpp
+++ src/main.cpp
@@ -155,7 +155,7 @@ void forceStart(){
     if(btnMode()){
         // Manual Alert System On
         Serial.println("Fatal Fall\n"); // Default to a Fatal Fall Type - Data send to app
-        fall_mode(); // Detects the fall
+        fall_mode(); 
     }
 }
 
